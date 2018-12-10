97cbfc007660f50ffe5105bb95af6b97e8ffd750
Tyrell Collins <ElectricMantis88@outlook.com>
2018-12-09 22:31:01 -0800

    RTC up and live

---
+++ clockUIFinal/clockUIFinal/Clock.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Final Project of Tyrell Collins ECET 230 2018
+
+namespace clockUIFinal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ClockPage : Page
+    {
+        DispatcherTimer Timer = new DispatcherTimer(); //create new instance of dispatch timer
+
+        public ClockPage()
+        {
+            InitializeComponent();
+            DataContext = this; 
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = new TimeSpan(0, 0, 1); //1 second interval
+            Timer.Start(); //start timer
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            Time.Text = DateTime.Now.ToString("h:mm:ss tt"); //Displays system time as a string in textblock
+        }
+    }
+}
